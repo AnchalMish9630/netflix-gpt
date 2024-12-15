c8f9637f671b444c2a122bf61bf7e6323d3e1959
Anchal Mishra <anchal.mishra@piramal.com>
2024-12-16 01:32:41 +0530

    net-gpt_2 form validation

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,3 +1,9 @@
-export const checkValidateData = () => {
-    
+export const checkValidateData = (email, password) => {
+    const isVaildEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+    const isValidPassword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$/.test(password);
+
+    if(!isVaildEmail) return "Email is not valid.";
+    if(!isValidPassword) return "Password is not valid";
+
+    return null;
 }
