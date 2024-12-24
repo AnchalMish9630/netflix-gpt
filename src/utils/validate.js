6b94daf4871b39f51ca727af2e1836401e9abf26
Anchal Mishra <anchal.mishra@piramal.com>
2024-12-24 23:43:28 +0530

    net-gpt_2 Fixing validaion

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,7 @@
 export const checkValidateData = (email, password) => {
     const isVaildEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
     const isValidPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,32}$/.test(password);
+    // const isNameValid = /([a-zA-Z0-9_\s]+)/.test(name);
 
     if(!isVaildEmail) return "Email is not valid.";
     if(!isValidPassword) return "Password is not valid - must be 8-32 characters and contain at least one number, uppercase letter, lowercase letter, and special character";
