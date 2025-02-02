99ee487669a114699e88308bd2d7f591b48d11c1
AnchalMish9630 <anchalmish2025@gmail.com>
2025-02-02 23:54:51 +0530

    net-gpt_5 Added secondary container

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux";
+import MovieCard from "./MovieCard";
+import MovieList from "./MovieList";
+ 
 const SecondaryContainer = ()=>{
+    const movies = useSelector(store=> store.movies)
+ 
     return(
-        <div>
-            SecondaryContainer
+        movies.nowPlayingMovies &&
+        <div className="bg-black">
+            <div className="-mt-60 pl-12 z-20 relative">
+            <MovieList title="Now Playing" movies={movies.nowPlayingMovies}/>
+            <MovieList title="Tredning" movies={movies.nowPlayingMovies}/>
+            <MovieList title="Horror" movies={movies.nowPlayingMovies}/>
+            <MovieList title="Now Playing" movies={movies.nowPlayingMovies}/>
+            <MovieList title="Now Playing" movies={movies.nowPlayingMovies}/>
+            <MovieList title="Now Playing" movies={movies.nowPlayingMovies}/>
+            </div>
         </div>
     )
 }
