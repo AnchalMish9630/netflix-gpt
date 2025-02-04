97faa8eca5dfe6bdec91de6f37e6d4b91e0d8bf6
AnchalMish9630 <anchalmish2025@gmail.com>
2025-02-05 00:15:38 +0530

    net-gpt_5 Added GPT search page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,25 +1,59 @@
 import { useState } from 'react';
 import { Header } from './Header';
-import useNowPlayingMovie from '../useNowPlayingMovie';
 import MainContainer from './MainContainer';
 import SecondaryContainer from './SecondaryContainer';
-
-
+import useNowPlayingMovie from '../hooks/useNowPlayingMovie';
+import useTopRatedMovie from '../hooks/useTopRatedMovie';
+import usePopularMovie from '../hooks/usePopularMovie';
+import useUpcomingMovie from '../hooks/useUpcomingMovie';
+import GptSearchPage from './GptSearchPage';
+import { useSelector } from 'react-redux';
+ 
+ 
 console.log("This is 2025..........First commit")
 const Browse = () => {
-
-  const [title] = useState([]);
+  // const [showGptSearch, setShowGptSearch] = useState(false);
+   const showGptSearch = useSelector(store=>store.gptSlice.showGptSearch);
 
   useNowPlayingMovie();
-
+  useTopRatedMovie();
+  usePopularMovie();
+  useUpcomingMovie();
+ 
   return (
     <div>
       <Header />
-      <MainContainer />
-      <SecondaryContainer />
+      {
+        showGptSearch ? (
+          <GptSearchPage />
+        ):(
+          <>
+           <MainContainer />
+           <SecondaryContainer />
+          </>
+        )
+      }
+      
+ 
+ 
+ 
+ 
+ 
+ 
+      {/* Logic for toggle using state variable.. */}
+ 
+      {/* <Header showGptSearch={showGptSearch} setShowGptSearch={setShowGptSearch} />
+      {showGptSearch ? (
+        <GptSearchPage.js />
+      ) : (
+        <>
+          <MainContainer />
+          <SecondaryContainer />
+        </>
+      )} */}
     </div>
    
   )
 }
-
+ 
 export default Browse
