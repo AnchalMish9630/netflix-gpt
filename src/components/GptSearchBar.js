8dde25628c57dccf211a44c7a7582165d319c29f
AnchalMish9630 <anchalmish2025@gmail.com>
2025-02-12 01:29:16 +0530

    net-gpt_5 Prevent default from form submission

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import { language } from "../utils/languageConstant";
 import { useSelector } from "react-redux";
+import openai from "../utils/openai";
 
 
 const GptSearchBar = ()=>{
+
+   
     const langKey = useSelector((store)=> store.config.language)
     return(
         <div className="pt-[10%] flex justify-center">
-             <form className='w-1/2 bg-black grid grid-cols-12'>
+             <form onSubmit={(e)=>e.preventDefault()}
+                className='w-1/2 bg-black grid grid-cols-12'>
             <input type="text" 
+                    ref={searchText}
                    placeholder={language[langKey].gptPlaceholder} 
                    className="p-4 m-4 col-span-9"/>
         
-            <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">{language[langKey].search}</button>
+            <button
+                onClick={onHandleSearchClick}
+                className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">
+                {language[langKey].search}
+            </button>
             </form>
         </div>
     )
